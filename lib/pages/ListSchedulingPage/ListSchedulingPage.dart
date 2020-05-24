d1d6099ff8146ac87787d2701657c429cb0c6334
William Terrill <william.terrill@gmail.com>
2020-05-23 20:26:25 -0500

    Development continues. made dialog for scheduling new audit... but just the types and the dates / times

--- lib/pages/ListSchedulingPage/ListSchedulingPage.dart
+++ lib/pages/ListSchedulingPage/ListSchedulingPage.dart
@@ -9,6 +9,7 @@ import 'package:auditor/pages/ListSchedulingPage/ApptDataTable/ApptDataTable.dar
 import 'package:flutter/material.dart';
 // import 'package:auditor/Definitions/Event.dart';
 import 'package:auditor/Definitions/colorDefs.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'TopDrawerWidget.dart';
 import 'TopWhiteHeaderWidget.dart';
@@ -63,34 +64,48 @@ class _ListSchedulingPageState extends State<ListSchedulingPage> {
                                     children: [
                                       // SizedBox(width: 50),
                                       FlatButton(
-                                          color: ColorDefs.colorAudit2,
-                                          shape: RoundedRectangleBorder(
-                                              borderRadius:
-                                                  BorderRadius.circular(18.0),
-                                              side: BorderSide(
-                                                  color: ColorDefs
-                                                      .colorAlternateDark)),
-                                          onPressed: () {
-                                            print("new audit pressed");
-                                          },
-                                          child: Text("Schedule New Audit",
-                                              style:
-                                                  ColorDefs.textBodyWhite20)),
+                                        color: ColorDefs.colorAudit2,
+                                        shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(18.0),
+                                            side: BorderSide(
+                                                color: ColorDefs
+                                                    .colorAlternateDark)),
+                                        onPressed: () {
+                                          showDialog<void>(
+                                            context: context,
+                                            builder: (BuildContext context) {
+                                              return AlertDialog(
+                                                content: StatefulBuilder(
+                                                  builder: (BuildContext
+                                                          context,
+                                                      StateSetter setState) {
+                                                    return NewAuditDialog();
+                                                    // return MyDialog();
+                                                  },
+                                                ),
+                                              );
+                                            },
+                                          );
+                                        },
+                                        child: Text("Schedule New Audit",
+                                            style: ColorDefs.textBodyWhite20),
+                                      ),
                                       // SizedBox(width: 10),
                                       FlatButton(
-                                          color: ColorDefs.colorAudit2,
-                                          shape: RoundedRectangleBorder(
-                                              borderRadius:
-                                                  BorderRadius.circular(18.0),
-                                              side: BorderSide(
-                                                  color: ColorDefs
-                                                      .colorAlternateDark)),
-                                          onPressed: () {
-                                            print("this week pressed");
-                                          },
-                                          child: Text("Show This Week",
-                                              style:
-                                                  ColorDefs.textBodyWhite20)),
+                                        color: ColorDefs.colorAudit2,
+                                        shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(18.0),
+                                            side: BorderSide(
+                                                color: ColorDefs
+                                                    .colorAlternateDark)),
+                                        onPressed: () {
+                                          print("Show this week pressed");
+                                        },
+                                        child: Text("Show This Week",
+                                            style: ColorDefs.textBodyWhite20),
+                                      ),
                                       // SizedBox(width: 10),
                                       SizedBox(
                                         height: 40,
@@ -139,6 +154,234 @@ class _ListSchedulingPageState extends State<ListSchedulingPage> {
           ],
         ),
       ),
+    );
+  }
+}
+
+class NewAuditDialog extends StatefulWidget {
+  NewAuditDialog({Key key}) : super(key: key);
+
+  @override
+  _NewAuditDialogState createState() => _NewAuditDialogState();
+}
+
+class _NewAuditDialogState extends State<NewAuditDialog> {
+  final _formKey = GlobalKey<FormState>();
+  String selectedAuditType = "Select";
+  String selectedProgType = "Select";
+  DateTime selectedDate = DateTime.now();
+  TimeOfDay selectedTime = TimeOfDay.now();
+
+  List<String> auditTypeDropDownMenu = [
+    "Select",
+    "Annual",
+    "Food Rescue",
+    "CEDA",
+    "Bi-Annual",
+    "Complaint",
+    "Follow Up",
+    "Grant"
+  ];
+
+  List<String> programTypeDropDownMenu = [
+    "Select",
+    "Healthy Student Market",
+    "Older Adults",
+    "Pantry",
+    "Congregate"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    print("doing build for New Audit Dialog");
+    List<DropdownMenuItem<String>> dropdown(List<String> stringArray) {
+      List<DropdownMenuItem<String>> dropDownList =
+          stringArray.map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList();
+      return dropDownList;
+    }
+
+    // String selectedFormattedTime = DateFormat.jm().format(DateTime.now());
+
+    return Stack(
+      overflow: Overflow.visible,
+      children: <Widget>[
+        Positioned(
+          right: -40.0,
+          top: -40.0,
+          child: InkResponse(
+            onTap: () {
+              Navigator.of(context).pop();
+            },
+            child: CircleAvatar(
+              child: Icon(Icons.close),
+              backgroundColor: Colors.red,
+            ),
+          ),
+        ),
+        Form(
+          key: _formKey,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Expanded(
+                              flex: 1,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  Text("Audit Type"),
+                                  Text("Program Type"),
+                                  Text("Date"),
+                                  Text("Start Time"),
+                                ],
+                              )),
+                          Expanded(
+                              flex: 1,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  DropdownButton<String>(
+                                    isExpanded: true,
+                                    value: selectedAuditType ?? "Select",
+                                    icon: Icon(Icons.arrow_downward),
+                                    iconSize: 24,
+                                    elevation: 16,
+                                    style: ColorDefs.textBodyBlack20,
+                                    underline: Container(
+                                      height: 2,
+                                      color: (selectedAuditType == "")
+                                          ? Colors.red
+                                          : Colors.green,
+                                    ),
+                                    onChanged: (String newValue) {
+                                      setState(() {
+                                        selectedAuditType = newValue;
+                                      });
+                                    },
+                                    items: dropdown(auditTypeDropDownMenu),
+                                  ),
+                                  DropdownButton<String>(
+                                    isExpanded: true,
+                                    value: selectedProgType ?? "Select",
+                                    icon: Icon(Icons.arrow_downward),
+                                    iconSize: 24,
+                                    elevation: 100,
+                                    style: ColorDefs.textBodyBlack20,
+                                    underline: Container(
+                                      height: 2,
+                                      color: (selectedProgType == "")
+                                          ? Colors.red
+                                          : Colors.green,
+                                    ),
+                                    onChanged: (String newValue) {
+                                      setState(() {
+                                        selectedProgType = newValue;
+                                      });
+                                    },
+                                    items: dropdown(programTypeDropDownMenu),
+                                  ),
+                                  GestureDetector(
+                                    onTap: () async {
+                                      DateTime selectedDatex;
+                                      selectedDatex = await showDatePicker(
+                                        context: context,
+                                        initialDate: DateTime.now(),
+                                        firstDate: DateTime.now(),
+                                        lastDate: DateTime(2030),
+                                        builder: (BuildContext context,
+                                            Widget child) {
+                                          return Theme(
+                                            data: ThemeData.dark(),
+                                            child: child,
+                                          );
+                                        },
+                                      );
+
+                                      setState(() {
+                                        selectedDate = selectedDatex;
+                                      });
+                                    },
+                                    child: Text(DateFormat('EEE MM-dd-yyyy')
+                                        .format(selectedDate)),
+                                    // selectedFormattedDate,
+                                    //     style: ColorDefs.textBodyBlue20),
+                                  ),
+                                  GestureDetector(
+                                    onTap: () async {
+                                      TimeOfDay selectedTimeRTL =
+                                          await showTimePicker(
+                                        context: context,
+                                        initialTime: TimeOfDay.now(),
+                                        // builder: (BuildContext context,
+                                        //     Widget child) {
+                                        //   return Directionality(
+                                        //     textDirection: TextDirection.LTR,
+                                        //     child: child,
+                                        //   );
+                                        // },
+                                      );
+                                      setState(() {
+                                        selectedTime = selectedTimeRTL;
+                                      });
+                                    },
+                                    child: Text(selectedTime
+                                        .format(context)
+                                        .toString()),
+                                  ),
+                                ],
+                              ))
+                        ],
+                      )
+                    ],
+                  )),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: TextFormField(),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: FlatButton(
+                  color: Colors.transparent,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                      side: BorderSide(color: ColorDefs.colorAudit2)),
+                  onPressed: () {
+                    if (_formKey.currentState.validate()) {
+                      _formKey.currentState.save();
+                      print(selectedDate);
+                      print(selectedTime.format(context).toString());
+
+                      // TimeOfDay t;
+                      // final now = new DateTime.now();
+                      DateTime selectedDateTime = DateTime(
+                          selectedDate.year,
+                          selectedDate.month,
+                          selectedDate.day,
+                          selectedTime.hour,
+                          selectedTime.minute);
+                      print(selectedDateTime.toString());
+
+                      Navigator.of(context).pop();
+                    }
+                  },
+                  child:
+                      Text("Schedule Audit", style: ColorDefs.textBodyBlue20),
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
